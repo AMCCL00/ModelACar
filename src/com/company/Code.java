9b4b4639601da187b35d8671dfc732c18981662b
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-31 14:04:31 -0500

    Creating Project

---
+++ src/com/company/Code.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Code {
+    Scanner ClassOfCar = new Scanner(System.in);
+    int Speed  = 0;
+    Scanner Year = new Scanner(System.in);
+    Scanner Color = new Scanner(System.in);
+    Scanner Make = new Scanner(System.in);
+    Scanner Model = new Scanner(System.in);
+    Scanner Type = new Scanner(System.in);
+
+    public void init (){
+
+    }
+
+}
---
+++ src/com/company/MessageUtil.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MessageUtil {
+
+    public static void Class(){
+        System.out.println("What type of car would you like? Pre-Built or Custom. \n");
+    }
+    public static void model (){
+        System.out.println("What model of car would you like? \n");
+    }
+    public static void year(){
+        System.out.println("What year of car would you like? \n");
+    }
+}
