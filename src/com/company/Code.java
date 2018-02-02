684076408f9a4e2d2d21f05778ec1ff03e7c31ca
Andrew McClary <andrew.mccalry00@gmail.com>
2018-02-02 15:30:43 -0500

    Premade Started

--- src/com/company/Code.java
+++ src/com/company/Code.java
@@ -5,7 +5,6 @@ public class Code {
     PremadeCar premadeCar = new PremadeCar();
     Custom custom = new Custom();
 
-    Scanner Class = new Scanner(System.in);
     Scanner Year = new Scanner(System.in);
     Scanner Color = new Scanner(System.in);
     Scanner Make = new Scanner(System.in);
@@ -13,7 +12,6 @@ public class Code {
     Scanner Type = new Scanner(System.in);
     int Speed  = 0;
     int year = Year.nextInt();
-    String input = Class.nextLine();
 
 
     public void init (){
--- src/com/company/PremadeCar.java
+++ src/com/company/PremadeCar.java
@@ -2,8 +2,12 @@ package com.company;
 
 public class PremadeCar {
 
+    String premadeType = "Car";
+    String premadeColor = "Silver";
+
     public void premade(){
-        System.out.println("The premade car is a ");
+        System.out.println("The premade vehicle is a " + premadeType + ".");
+        System.out.println("The premade " + premadeType + " is " + premadeColor + ".");
     }
 
 }
